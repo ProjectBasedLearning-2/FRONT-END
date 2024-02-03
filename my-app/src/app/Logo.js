8480971bdc35b56846933d5086875eb62bbde201
Kunals <kunalshinde0612@gmail.com>
2024-02-03 09:38:22 +0530

    commited

--- my-app/src/app/Logo.js
+++ my-app/src/app/Logo.js
@@ -6,7 +6,7 @@ export default function Logo() {
     <div className='w-[36px] absolute top-[9%] left-[6%]'>
       <img src="https://cdn-icons-png.freepik.com/256/1327/1327238.png?uid=R91415930&ga=GA1.1.1299523950.1706704366&semt=ais"></img>
     </div>
-    <p className='absolute font-semibold top-[9%] left-[9%] text-[24px]'>Green<span className='text-[#43C949]'>Genius</span></p>
+    <p className='absolute font-semibold top-[9%] left-[9%] text-[30px]'>Green<span className='text-[#43C949]'>Genius</span></p>
     </>
   )
 }
